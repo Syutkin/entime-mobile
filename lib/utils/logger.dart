f6075abc377001dd4bb7b3f80361dd225f238453
Andrey Syutkin <syutkin@gmail.com>
2021-11-14 21:36:55 +0300

    Logger PrettyPrinter format

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,5 +1,11 @@
 import 'package:logger/logger.dart';
 
 var logger = Logger(
-  printer: PrettyPrinter(printTime: true),
+  printer: PrettyPrinter(
+    printTime: false,
+    colors: false,
+    noBoxingByDefault: true,
+    printEmojis: false,
+    methodCount: 0,
+  ),
 );
