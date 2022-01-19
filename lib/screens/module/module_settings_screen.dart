52dfe0926445b1ece695f7f110fcc8b6a3f31806
Andrey Syutkin <syutkin@gmail.com>
2022-01-19 10:32:18 +0300

    Fix import and change maven repo

--- lib/screens/module/module_settings_screen.dart
+++ lib/screens/module/module_settings_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_settings_ui/flutter_settings_ui.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+// import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import '../../blocs/blocs.dart';
 import '../../widgets/splash.dart';
