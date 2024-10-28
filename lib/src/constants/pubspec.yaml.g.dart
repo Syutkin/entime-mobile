57314d8929a42cc648644f50ba705c4f6e5cde29
Andrey Syutkin <syutkin@gmail.com>
2024-10-28 13:47:02 +0300

    Update Codecov github actions

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.0-alpha.1+279',
+    representation: r'0.5.0-alpha.1+280',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.0-alpha.1+279',
+    canonical: r'0.5.0-alpha.1+280',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,7 +118,7 @@ sealed class Pubspec {
     preRelease: <String>[r'alpha', r'1'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'279'],
+    build: <String>[r'280'],
   );
 
   /// Build date and time (UTC)
@@ -127,10 +127,10 @@ sealed class Pubspec {
     10,
     28,
     10,
-    35,
-    27,
-    152,
-    665,
+    47,
+    44,
+    842,
+    421,
   );
 
   /// Name
