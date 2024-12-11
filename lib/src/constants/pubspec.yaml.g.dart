b6ac433bc39513fb7083a0a097c52d62feee481b
Andrey Syutkin <syutkin@gmail.com>
2024-12-11 08:46:14 +0300

    Fix release workflow

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.0-alpha.2+344',
+    representation: r'0.5.0-alpha.2+345',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.0-alpha.2+344',
+    canonical: r'0.5.0-alpha.2+345',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,7 +118,7 @@ sealed class Pubspec {
     preRelease: <String>[r'alpha', r'2'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'344'],
+    build: <String>[r'345'],
   );
 
   /// Build date and time (UTC)
@@ -127,10 +127,10 @@ sealed class Pubspec {
     12,
     11,
     5,
-    26,
-    50,
-    686,
-    800,
+    46,
+    35,
+    11,
+    425,
   );
 
   /// Name
