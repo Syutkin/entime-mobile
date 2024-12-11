3ba43883234870f1237f04f1d813d261e02606a9
Andrey Syutkin <syutkin@gmail.com>
2024-12-11 07:58:23 +0300

    Flutter github test

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.0-alpha.1+338',
+    representation: r'0.5.0-alpha.1+339',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.0-alpha.1+338',
+    canonical: r'0.5.0-alpha.1+339',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,7 +118,7 @@ sealed class Pubspec {
     preRelease: <String>[r'alpha', r'1'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'338'],
+    build: <String>[r'339'],
   );
 
   /// Build date and time (UTC)
@@ -127,10 +127,10 @@ sealed class Pubspec {
     12,
     11,
     4,
-    53,
-    24,
-    204,
-    769,
+    58,
+    42,
+    74,
+    498,
   );
 
   /// Name
