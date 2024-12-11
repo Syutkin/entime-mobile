1eecb4527d266e1c5430155868330beb76638907
Andrey Syutkin <syutkin@gmail.com>
2024-12-11 03:30:08 +0300

    Fix github CI/CD

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.0-alpha.1+326',
+    representation: r'0.5.0-alpha.1+327',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.0-alpha.1+326',
+    canonical: r'0.5.0-alpha.1+327',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,19 +118,19 @@ sealed class Pubspec {
     preRelease: <String>[r'alpha', r'1'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'326'],
+    build: <String>[r'327'],
   );
 
   /// Build date and time (UTC)
   static final DateTime timestamp = DateTime.utc(
     2024,
     12,
-    10,
-    23,
-    39,
-    15,
-    650,
-    158,
+    11,
+    0,
+    30,
+    26,
+    390,
+    656,
   );
 
   /// Name
