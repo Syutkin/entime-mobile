afa721643c3cc5ef003413786b1347151c9c88ee
Andrey Syutkin <syutkin@gmail.com>
2025-01-21 15:37:14 +0300

    Some RaceTile tests

---
+++ test/src/feature/tab/widget/race_tile_test.dart
@@ -0,0 +1,123 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:entime/src/common/localization/localization.dart';
+import 'package:entime/src/feature/database/database.dart';
+import 'package:entime/src/feature/tab/widget/race_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:patrol_finders/patrol_finders.dart';
+
+class MockDatabaseBloc extends MockBloc<DatabaseEvent, DatabaseState>
+    implements DatabaseBloc {}
+
+void main() {
+  late DatabaseBloc databaseBloc;
+  late Race race;
+  late Stage stage;
+
+  Widget testWidget() {
+    return MaterialApp(
+      localizationsDelegates: const [Localization.delegate],
+      supportedLocales: Localization.supportedLocales,
+      home: Material(
+        child: BlocProvider.value(
+          value: databaseBloc,
+          child: const RaceTile(),
+        ),
+      ),
+    );
+  }
+
+  setUpAll(
+    () {
+      databaseBloc = MockDatabaseBloc();
+    },
+  );
+
+  setUp(
+    () {
+      race = const Race(id: 1, name: 'Race name', isDeleted: false);
+      stage = const Stage(
+        id: 1,
+        raceId: 1,
+        name: 'Stage name',
+        isActive: true,
+        isDeleted: false,
+      );
+      when(() => databaseBloc.state).thenReturn(
+        const DatabaseState(
+          races: [],
+          stages: [],
+          categories: [],
+          riders: [],
+          participants: [],
+          starts: [],
+          finishes: [],
+          numbersOnTrace: [],
+        ),
+      );
+    },
+  );
+
+  group(
+    'RaceTile tests',
+    () {
+      patrolWidgetTest('Initial build', (
+        PatrolTester $,
+      ) async {
+        await $.pumpWidgetAndSettle(testWidget());
+        expect($(RaceTile), findsOneWidget);
+        expect($(ListTile), findsOneWidget);
+      });
+
+      patrolWidgetTest('Race selected and stage not selected', (
+        PatrolTester $,
+      ) async {
+        when(() => databaseBloc.state).thenReturn(
+          DatabaseState(
+            races: [],
+            stages: [],
+            categories: [],
+            riders: [],
+            participants: [],
+            starts: [],
+            finishes: [],
+            numbersOnTrace: [],
+            race: race,
+          ),
+        );
+
+        await $.pumpWidgetAndSettle(testWidget());
+        expect($(Localization.current.I18nInit_selectRace), findsNothing);
+        expect($(race.name), findsOneWidget);
+        expect($(Localization.current.I18nInit_selectStage), findsOneWidget);
+      });
+
+      patrolWidgetTest('Race and stage selected', (
+        PatrolTester $,
+      ) async {
+        when(() => databaseBloc.state).thenReturn(
+          DatabaseState(
+            races: [],
+            stages: [],
+            categories: [],
+            riders: [],
+            participants: [],
+            starts: [],
+            finishes: [],
+            numbersOnTrace: [],
+            race: race,
+            stage: stage,
+          ),
+        );
+
+        await $.pumpWidgetAndSettle(testWidget());
+        expect($(Localization.current.I18nInit_selectRace), findsNothing);
+        expect($(race.name), findsOneWidget);
+        expect($(Localization.current.I18nInit_selectStage), findsNothing);
+        expect($(stage.name), findsOneWidget);
+      });
+    },
+  );
+}
