afa6e0373684255bd5096a7cb2d44b6f87fb3931
Andrey Syutkin <syutkin@gmail.com>
2025-02-03 11:50:07 +0300

    Remove verbose from build command

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.0+425',
+    representation: r'0.5.0+426',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.0+425',
+    canonical: r'0.5.0+426',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,7 +118,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'425'],
+    build: <String>[r'426'],
   );
 
   /// Build date and time (UTC)
@@ -127,10 +127,10 @@ sealed class Pubspec {
     2,
     3,
     8,
-    35,
-    56,
-    765,
-    380,
+    50,
+    29,
+    212,
+    988,
   );
 
   /// Name
