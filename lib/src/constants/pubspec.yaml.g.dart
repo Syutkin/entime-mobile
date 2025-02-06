c70f316e0ec6e71c30f3093706647c8469bd71a7
Andrey Syutkin <syutkin@gmail.com>
2025-02-06 18:34:11 +0300

    Update changelog

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -93,13 +93,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.5.1-dev+434',
+    representation: r'0.5.1-dev+435',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.5.1-dev+434',
+    canonical: r'0.5.1-dev+435',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -118,7 +118,7 @@ sealed class Pubspec {
     preRelease: <String>[r'dev'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'434'],
+    build: <String>[r'435'],
   );
 
   /// Build date and time (UTC)
@@ -127,10 +127,10 @@ sealed class Pubspec {
     2,
     6,
     15,
-    30,
-    19,
-    803,
-    480,
+    34,
+    31,
+    714,
+    771,
   );
 
   /// Name
