02ebc505c3e51adb69aee9b06c3618a1e46b85d0
Andrey Syutkin <syutkin@gmail.com>
2025-05-23 17:54:47 +0300

    Update changelog

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'0.5.1-dev+485';
+const String version = r'0.5.1-dev+486';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 0;
@@ -55,10 +55,10 @@ const int patch = 1;
 const List<String> pre = <String>[r'dev'];
 
 /// The build identifier: "foo" in "1.2.3+foo".
-const List<String> build = <String>[r'485'];
+const List<String> build = <String>[r'486'];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1748009653;
+const int timestamp = 1748012140;
 
 /// Name [name]
 const String name = r'entime';
