b43ecd7a6502bae5a7de90c5de05c34349746f0f
Andrey Syutkin <syutkin@gmail.com>
2025-05-23 20:02:59 +0300

    compileSdkVersion 36

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'0.5.1+488';
+const String version = r'0.5.1+489';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 0;
@@ -55,10 +55,10 @@ const int patch = 1;
 const List<String> pre = <String>[];
 
 /// The build identifier: "foo" in "1.2.3+foo".
-const List<String> build = <String>[r'488'];
+const List<String> build = <String>[r'489'];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1748018714;
+const int timestamp = 1748019844;
 
 /// Name [name]
 const String name = r'entime';
