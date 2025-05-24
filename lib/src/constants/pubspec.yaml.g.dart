97cfa08dacb5fd92a01742ce6dec4f630339c7f0
Andrey Syutkin <syutkin@gmail.com>
2025-05-24 08:19:51 +0300

    Revert "Accept android-licenses?"
    This reverts commit 430aec2a5b098510a8abfb906059ccc4ba626c80.

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'0.5.1+488';
+const String version = r'0.5.1+487';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 0;
@@ -55,10 +55,10 @@ const int patch = 1;
 const List<String> pre = <String>[];
 
 /// The build identifier: "foo" in "1.2.3+foo".
-const List<String> build = <String>[r'488'];
+const List<String> build = <String>[r'487'];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1748018714;
+const int timestamp = 1748012479;
 
 /// Name [name]
 const String name = r'entime';
