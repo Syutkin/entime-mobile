3d1f0e09d4641b34393f9b9e9c9b3a602e12043a
Andrey Syutkin <syutkin@gmail.com>
2025-05-29 13:08:19 +0300

    Enable module settings wifi and ssid

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'0.5.1+487';
+const String version = r'0.5.1+488';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 0;
@@ -55,10 +55,10 @@ const int patch = 1;
 const List<String> pre = <String>[];
 
 /// The build identifier: "foo" in "1.2.3+foo".
-const List<String> build = <String>[r'487'];
+const List<String> build = <String>[r'488'];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1748012479;
+const int timestamp = 1748513384;
 
 /// Name [name]
 const String name = r'entime';
