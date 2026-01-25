9fba8f3ed4b304132a1b319c959324721ec3b801
Andrey Syutkin <syutkin@gmail.com>
2026-01-25 18:49:53 +0300

    Linux build

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.6.0-dev+566',
+    representation: r'0.6.0-dev+567',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.6.0-dev+566',
+    canonical: r'0.6.0-dev+567',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,19 +117,19 @@ sealed class Pubspec {
     preRelease: <String>[r'dev'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'566'],
+    build: <String>[r'567'],
   );
 
   /// Build date and time (UTC)
   static final DateTime timestamp = DateTime.utc(
     2026,
     1,
-    23,
-    18,
-    45,
-    47,
-    39,
-    273,
+    25,
+    15,
+    50,
+    1,
+    221,
+    618,
   );
 
   /// Name
