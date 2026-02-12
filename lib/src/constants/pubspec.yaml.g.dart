fa4ee8fbc8f0d5efe524e9dcd56d7be2a8b0645b
Andrey Syutkin <syutkin@gmail.com>
2026-02-12 23:54:34 +0300

    Relax analyzer rule for cascade_invocations

--- lib/src/constants/pubspec.yaml.g.dart
+++ lib/src/constants/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'0.6.0-dev+610',
+    representation: r'0.6.0-dev+611',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'0.6.0-dev+610',
+    canonical: r'0.6.0-dev+611',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[r'dev'],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'610'],
+    build: <String>[r'611'],
   );
 
   /// Build date and time (UTC)
@@ -127,9 +127,9 @@ sealed class Pubspec {
     12,
     20,
     54,
-    17,
-    617,
-    938,
+    41,
+    699,
+    25,
   );
 
   /// Name
