37fd8395a4f79ac2aa1296baa81fe6cad2990db2
Andrey Syutkin <syutkin@gmail.com>
2026-05-19 20:06:13 +0300

    settings_ui version bump

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -10,7 +10,7 @@ cider bump build
 dart pub global activate pubspec_generator
 dart pub global run pubspec_generator:generate --input pubspec.yaml --output lib/src/constants/pubspec.yaml.g.dart
 
-dart run build_runner build --delete-conflicting-outputs
+dart run build_runner build
 
 git add pubspec.yaml
 git add -- "lib/**/*.freezed.dart" "lib/**/*.g.dart" "lib/**/*.steps.dart" "lib/src/common/localization/**/*.dart"
