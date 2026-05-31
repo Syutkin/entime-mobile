572a5ad35530d3b5a9cf2b92831f373a4dda82bd
Andrey Syutkin <syutkin@gmail.com>
2026-05-31 12:47:04 +0300

    Update release workflow to build linux artifact

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,19 @@
-flutter build apk --target-platform android-arm,android-arm64,android-x64 --split-per-abi
+#!/usr/bin/env bash
+set -euo pipefail
+
+release_version="${1:-local}"
+artifact_dir="build/release"
+
+flutter build apk --release --split-per-abi
+flutter build apk --release --target-platform android-arm,android-arm64,android-x64
+flutter build linux --release
+
+mkdir -p "${artifact_dir}"
+cp build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk "${artifact_dir}/entime-${release_version}-armeabi-v7a.apk"
+cp build/app/outputs/flutter-apk/app-arm64-v8a-release.apk "${artifact_dir}/entime-${release_version}-arm64-v8a.apk"
+cp build/app/outputs/flutter-apk/app-x86_64-release.apk "${artifact_dir}/entime-${release_version}-x86_64.apk"
+cp build/app/outputs/flutter-apk/app-release.apk "${artifact_dir}/entime-${release_version}-universal.apk"
+sha1sum "${artifact_dir}"/entime-"${release_version}"-*.apk > "${artifact_dir}/entime-${release_version}-apk.sha1"
+
+tar -C build/linux/x64/release --transform 's,^bundle,entime,' -czf "${artifact_dir}/entime-${release_version}-linux-x64.tar.gz" bundle
+sha1sum "${artifact_dir}/entime-${release_version}-linux-x64.tar.gz" > "${artifact_dir}/entime-${release_version}-linux-x64.tar.gz.sha1"
